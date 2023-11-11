6596d49032a4da4e3085592fddc17364524d5aac
Deepika Merlin N <144902700+deepikamerlin@users.noreply.github.com>
2023-11-11 07:24:41 +0530

    Add files via upload

---
+++ LeapYearChecker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class LeapYearChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+        scanner.close();
+    }
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
